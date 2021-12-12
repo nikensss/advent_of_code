f3243cb5675ffb2abde7be2e76cd043e884f83f1
Ricard Mallafre <rikitzzz@gmail.com>
2021-12-12 16:39:57 +0100

    feat: day 1, part 2 in rust

--- rust/day_1/src/main.rs
+++ rust/day_1/src/main.rs
@@ -1,10 +1,6 @@
 use std::fs;
 
 fn main() {
-    part_1();
-}
-
-fn part_1() {
     let filename = "input.txt";
 
     let contents = fs::read_to_string(filename).expect("Could not read file");
@@ -13,6 +9,11 @@ fn part_1() {
         .map(|x| x.parse::<u32>().expect("Not a number"))
         .collect::<Vec<u32>>();
 
+    part_1(&contents);
+    part_2(&contents);
+}
+
+fn part_1(contents: &Vec<u32>) {
     let mut increases = 0;
 
     for i in 0..contents.len() - 1 {
@@ -22,4 +23,14 @@ fn part_1() {
     }
 
     println!("Increases: {}", increases)
+}
+
+fn part_2(contents: &Vec<u32>) {
+    let mut rolling_window: Vec<u32> = Vec::new();
+
+    for i in 0..contents.len() - 2 {
+        rolling_window.push(contents[i] + contents[i + 1] + contents[i + 2])
+    }
+
+    part_1(&rolling_window)
 }
