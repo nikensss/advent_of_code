5b6241067cc0dc8c43596aca42fbbf9dfecd6df0
Ricard Mallafre <rikitzzz@gmail.com>
2022-01-08 19:26:33 +0100

    chore: small changes

--- rust/day_3/src/main.rs
+++ rust/day_3/src/main.rs
@@ -3,45 +3,45 @@ use std::fs;
 fn main() {
     let filename = "input.txt";
 
-    let contents = fs::read_to_string(filename).expect("Could not read file");
-    let contents: Vec<&str> = contents.lines().collect();
+    let file_contents = fs::read_to_string(filename).expect("Could not read file");
+    let lines: Vec<&str> = file_contents.lines().collect();
 
-    let gamma = calculate_gamma(&contents);
-    let epsilon = calculate_epsilon(&contents);
+    let gamma = calculate_gamma(&lines);
+    let epsilon = calculate_epsilon(&lines);
 
-    let oxygen = calculate_oxygen(&contents);
-    let co2 = calculate_co2(&contents);
+    let oxygen = calculate_oxygen(&lines);
+    let co2 = calculate_co2(&lines);
 
     println!("{} * {} = {}", gamma, epsilon, gamma * epsilon);
     println!("{} * {} = {}", oxygen, co2, oxygen * co2);
 }
 
-fn find_most_common<'a>(contents: &'a Vec<&str>) -> Vec<&'a str> {
-    count_bit_occurrence(&contents)
+fn find_most_common<'a>(lines: &'a Vec<&str>) -> Vec<&'a str> {
+    count_bit_occurrence(&lines)
         .into_iter()
         .map(|x| if x >= 0 { "1" } else { "0" })
         .collect::<Vec<&str>>()
 }
 
-fn find_least_common<'a>(contents: &'a Vec<&str>) -> Vec<&'a str> {
-    count_bit_occurrence(&contents)
+fn find_least_common<'a>(lines: &'a Vec<&str>) -> Vec<&'a str> {
+    count_bit_occurrence(&lines)
         .into_iter()
         .map(|x| if x >= 0 { "0" } else { "1" })
         .collect::<Vec<&str>>()
 }
 
-fn count_bit_occurrence(contents: &Vec<&str>) -> Vec<i32> {
-    let mut count: Vec<i32> = vec![0; contents[0].len()];
+fn count_bit_occurrence(lines: &Vec<&str>) -> Vec<i32> {
+    let mut count: Vec<i32> = vec![0; lines[0].len()];
     for i in 0..count.len() {
-        count[i] = count_bit_occurrence_at(&contents, i);
+        count[i] = count_bit_occurrence_at(&lines, i);
     }
 
     count
 }
 
-fn count_bit_occurrence_at(contents: &Vec<&str>, index: usize) -> i32 {
+fn count_bit_occurrence_at(lines: &Vec<&str>, index: usize) -> i32 {
     let mut occurence = 0;
-    for line in contents {
+    for line in lines {
         let character = &line[index..index + 1];
         match character {
             "0" => occurence -= 1,
@@ -53,9 +53,9 @@ fn count_bit_occurrence_at(contents: &Vec<&str>, index: usize) -> i32 {
     occurence
 }
 
-fn calculate_gamma(contents: &Vec<&str>) -> isize {
+fn calculate_gamma(lines: &Vec<&str>) -> isize {
     let mut gamma_binary = String::from("");
-    let most_common = find_most_common(&contents);
+    let most_common = find_most_common(&lines);
     for c in most_common {
         gamma_binary.push_str(c);
     }
@@ -63,9 +63,9 @@ fn calculate_gamma(contents: &Vec<&str>) -> isize {
     isize::from_str_radix(&gamma_binary, 2).unwrap()
 }
 
-fn calculate_epsilon(contents: &Vec<&str>) -> isize {
+fn calculate_epsilon(lines: &Vec<&str>) -> isize {
     let mut epsilon_binary = String::from("");
-    let least_common = find_least_common(&contents);
+    let least_common = find_least_common(&lines);
     for c in least_common {
         epsilon_binary.push_str(c);
     }
@@ -73,8 +73,8 @@ fn calculate_epsilon(contents: &Vec<&str>) -> isize {
     isize::from_str_radix(&epsilon_binary, 2).unwrap()
 }
 
-fn calculate_oxygen(contents: &Vec<&str>) -> isize {
-    let mut oxygen_candidates = contents.clone();
+fn calculate_oxygen(lines: &Vec<&str>) -> isize {
+    let mut oxygen_candidates = lines.clone();
     let total_bits = oxygen_candidates[0].len();
     for i in 0..total_bits {
         if oxygen_candidates.len() == 1 {
@@ -92,8 +92,8 @@ fn calculate_oxygen(contents: &Vec<&str>) -> isize {
     isize::from_str_radix(&oxygen_candidates[0], 2).unwrap()
 }
 
-fn calculate_co2(contents: &Vec<&str>) -> isize {
-    let mut co2_candidates: Vec<&str> = contents.clone();
+fn calculate_co2(lines: &Vec<&str>) -> isize {
+    let mut co2_candidates: Vec<&str> = lines.clone();
     let total_bits = co2_candidates[0].len();
     for i in 0..total_bits {
         if co2_candidates.len() == 1 {
@@ -119,11 +119,11 @@ mod test {
     fn should_match_part1_solution() {
         let filename = "input.txt";
 
-        let contents = fs::read_to_string(filename).expect("Could not read file");
-        let contents: Vec<&str> = contents.lines().collect();
+        let file_contents = fs::read_to_string(filename).expect("Could not read file");
+        let lines: Vec<&str> = file_contents.lines().collect();
 
-        let gamma = calculate_gamma(&contents);
-        let epsilon = calculate_epsilon(&contents);
+        let gamma = calculate_gamma(&lines);
+        let epsilon = calculate_epsilon(&lines);
 
         assert_eq!(gamma * epsilon, 2595824);
     }
@@ -132,11 +132,11 @@ mod test {
     fn should_match_part2_solution() {
         let filename = "input.txt";
 
-        let contents = fs::read_to_string(filename).expect("Could not read file");
-        let contents: Vec<&str> = contents.lines().collect();
+        let file_contents = fs::read_to_string(filename).expect("Could not read file");
+        let lines: Vec<&str> = file_contents.lines().collect();
 
-        let oxygen = calculate_oxygen(&contents);
-        let co2 = calculate_co2(&contents);
+        let oxygen = calculate_oxygen(&lines);
+        let co2 = calculate_co2(&lines);
 
         assert_eq!(oxygen * co2, 2135254);
     }
