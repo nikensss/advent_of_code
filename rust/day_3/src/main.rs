c0ff5b6e9cdb165570605f5dff7516d19f95304b
Ricard Mallafre <rikitzzz@gmail.com>
2022-01-08 18:28:10 +0100

    feat: solution day 3, part 2

--- rust/day_3/src/main.rs
+++ rust/day_3/src/main.rs
@@ -10,7 +10,11 @@ fn main() {
     let gamma = calculate_gamma(&count);
     let epsilon = calculate_epsilon(&count);
 
-    println!("{}", gamma * epsilon)
+    let oxygen = calculate_oxygen(&contents);
+    let co2 = calculate_co2(&contents);
+
+    println!("{} * {} = {}", gamma, epsilon, gamma * epsilon);
+    println!("{} * {} = {}", oxygen, co2, oxygen * co2);
 }
 
 fn count_bit_occurrence(contents: &Vec<&str>) -> Vec<i32> {
@@ -49,6 +53,44 @@ fn calculate_epsilon(count: &Vec<i32>) -> isize {
     isize::from_str_radix(&epsilon_binary, 2).unwrap()
 }
 
+fn calculate_oxygen(contents: &Vec<&str>) -> isize {
+    let mut oxygen_candidates = contents.clone();
+    let total_bits = oxygen_candidates[0].len();
+    for i in 0..total_bits {
+        if oxygen_candidates.len() == 1 {
+            break;
+        }
+
+        let moving_count = count_bit_occurrence(&oxygen_candidates);
+        let most_common = if moving_count[i] >= 0 { "1" } else { "0" };
+        oxygen_candidates = oxygen_candidates
+            .into_iter()
+            .filter(|candidate| &candidate[i..i + 1] == most_common)
+            .collect::<Vec<&str>>();
+    }
+
+    isize::from_str_radix(&oxygen_candidates[0], 2).unwrap()
+}
+
+fn calculate_co2(contents: &Vec<&str>) -> isize {
+    let mut co2_candidates: Vec<&str> = contents.clone();
+    let total_bits = co2_candidates[0].len();
+    for i in 0..total_bits {
+        if co2_candidates.len() == 1 {
+            break;
+        }
+
+        let moving_count = count_bit_occurrence(&co2_candidates);
+        let least_common = if moving_count[i] >= 0 { "0" } else { "1" };
+        co2_candidates = co2_candidates
+            .into_iter()
+            .filter(|candidate| &candidate[i..i + 1] == least_common)
+            .collect::<Vec<&str>>();
+    }
+
+    isize::from_str_radix(&co2_candidates[0], 2).unwrap()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -65,5 +107,18 @@ mod test {
         let epsilon = calculate_epsilon(&count);
 
         assert_eq!(gamma * epsilon, 2595824);
+    }
+
+    #[test]
+    fn should_match_part2_solution() {
+        let filename = "input.txt";
+
+        let contents = fs::read_to_string(filename).expect("Could not read file");
+        let contents: Vec<&str> = contents.lines().collect();
+
+        let oxygen = calculate_oxygen(&contents);
+        let co2 = calculate_co2(&contents);
+
+        assert_eq!(oxygen * co2, 2135254);
     }
 }
