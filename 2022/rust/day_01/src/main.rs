df72713c215858f05730a5d63c42dc74d277649e
Ricard Mallafre <ricard.mallafre@basic-fit.com>
2022-12-02 10:39:32 +0100

    feat: solutions to day 1

---
+++ 2022/rust/day_01/src/main.rs
@@ -0,0 +1,57 @@
+use std::fs;
+
+fn main() {
+    let filename = "input_01.txt";
+    let calories = fs::read_to_string(filename).expect("Could not read file");
+
+    let res_1 = part_1(&calories);
+    println!(
+        "{} ({})",
+        res_1,
+        if res_1 == 71506 { "ok" } else { "wrong" }
+    );
+
+    let res_2 = part_2(&calories);
+    println!(
+        "{} ({})",
+        res_2,
+        if res_2 == 209603 { "ok" } else { "wrong" }
+    );
+}
+
+fn part_1(calories: &str) -> u32 {
+    let mut current_count = 0;
+    let mut max = 0;
+
+    for line in calories.lines() {
+        if let Ok(n) = line.parse::<u32>() {
+            current_count += n;
+        } else {
+            max = if current_count > max {
+                current_count
+            } else {
+                max
+            };
+            current_count = 0;
+        };
+    }
+
+    max
+}
+
+fn part_2(calories: &str) -> u32 {
+    let mut current_count = 0;
+    let mut calorie_count = Vec::new();
+
+    for line in calories.lines() {
+        if let Ok(n) = line.parse::<u32>() {
+            current_count += n;
+        } else {
+            calorie_count.push(current_count);
+            current_count = 0;
+        };
+    }
+
+    calorie_count.sort();
+    calorie_count.iter().rev().take(3).sum()
+}
