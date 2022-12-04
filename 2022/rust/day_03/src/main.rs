d3ce27005a59e3e0cac60decda4254ffa541a00a
Ricard Mallafre <rikitzzz@gmail.com>
2022-12-04 18:56:41 +0100

    feat: day 3 part 2

--- 2022/rust/day_03/src/main.rs
+++ 2022/rust/day_03/src/main.rs
@@ -5,35 +5,38 @@ fn main() {
     let lines = fs::read_to_string(filename).expect("Cannot read file");
     let lines: Vec<&str> = lines.lines().collect();
 
-    let res_part_1 = part_1(lines);
-    let is_ok = if res_part_1 == 7826 { "ok" } else { "wrong" };
-    println!("{} ({})", res_part_1, is_ok);
+    let res_part_1 = part_1(&lines);
+    let is_part_1_ok = if res_part_1 == 7826 { "ok" } else { "wrong" };
+    println!("{} ({})", res_part_1, is_part_1_ok);
+
+    let res_part_2 = part_2(&lines);
+    let is_part_2_ok = if res_part_2 == 2577 { "ok" } else { "wrong" };
+    println!("{} ({})", res_part_2, is_part_2_ok);
 }
 
-fn part_1(lines: Vec<&str>) -> u32 {
+fn part_1(lines: &Vec<&str>) -> u32 {
     let mut total = 0;
     for line in lines {
         match find_repeated_char(line) {
             Ok(c) => total += char_to_priority(c),
-            Err(msg) => println!("{}", msg),
+            Err(_) => println!("No repeated char: {}", line),
         }
     }
 
     total
 }
 
-fn find_repeated_char(line: &str) -> Result<char, &str> {
+fn find_repeated_char(line: &str) -> Result<char, ()> {
     if line.len() % 2 != 0 {
-        return Err("line cannot be divided in 2");
+        return Err(());
     }
 
     let upper: HashSet<char> = line[0..line.len() / 2].chars().collect();
     let lower: HashSet<char> = line[line.len() / 2..line.len()].chars().collect();
 
-    let mut intersection = upper.intersection(&lower);
-    match intersection.next() {
+    match upper.intersection(&lower).next() {
         Some(c) => Ok(*c),
-        None => Err("no interesection"),
+        None => Err(()),
     }
 }
 
@@ -42,5 +45,28 @@ fn char_to_priority(c: char) -> u32 {
         c as u32 - 'a' as u32 + 1
     } else {
         c as u32 - 'A' as u32 + 1 + 26
+    }
+}
+
+fn part_2(lines: &Vec<&str>) -> u32 {
+    let mut total = 0;
+    for i in (0..lines.len()).step_by(3) {
+        let group = (lines[i], lines[i + 1], lines[i + 2]);
+        let priority = char_to_priority(find_group_badge(group).expect("No intersection"));
+        total += priority;
+    }
+
+    total
+}
+
+fn find_group_badge(group: (&str, &str, &str)) -> Result<char, ()> {
+    let a: HashSet<char> = group.0.chars().collect();
+    let b: HashSet<char> = group.1.chars().collect();
+    let c: HashSet<char> = group.2.chars().collect();
+
+    let intersection_of_a_and_b = a.intersection(&b).cloned().collect::<HashSet<char>>();
+    match intersection_of_a_and_b.intersection(&c).next() {
+        Some(letter) => Ok(*letter),
+        None => Err(()),
     }
 }
