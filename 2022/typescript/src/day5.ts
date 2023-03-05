cb1762f8e8f2c44df9bb7a6e5d1ad617e87a0319
Ricard Mallafre <rikitzzz@gmail.com>
2023-03-05 17:20:16 +0100

    feat: day 5, part 2, typescript solution

--- 2022/typescript/src/day5.ts
+++ 2022/typescript/src/day5.ts
@@ -1,17 +1,17 @@
 import { promises as fs } from 'fs';
 
 export const day5 = async (): Promise<void> => {
-  const lines = (await fs.readFile('./src/day5.input.txt', 'utf8')).split('\n');
-  const resultPart1 = day5Part1(lines);
-  console.log({ day5part1: resultPart1 });
-  // const resultPart2 = day5Part2(lines);
-  // console.log({resultPart2});
+  const input = new Input((await fs.readFile('./src/day5.input.txt', 'utf8')).split('\n'));
+  const resultPart1 = day5Part1(input);
+  console.log({ day5part1: resultPart1, isCorrect: resultPart1 === 'DHBJQJCCW' });
+  const resultPart2 = day5Part2(input);
+  console.log({ day5Part2: resultPart2, isCorrect: resultPart2 === 'WJVRLSJJT' });
 };
 
-const day5Part1 = (lines: string[]): string => {
-  const crateStacks = CrateStack.fromString(getCratesSection(lines));
-  const moves = getMovesSection(lines).map(move => new Move(move));
-  const crane = new Crane(crateStacks);
+const day5Part1 = (input: Input): string => {
+  const crateStacks = CrateStack.parse(input.getCratesSection());
+  const moves = Move.parse(input.getMovesSection());
+  const crane = Crane.getCrateMover9000(crateStacks);
   for (const move of moves) {
     crane.move(move);
   }
@@ -22,23 +22,39 @@ const day5Part1 = (lines: string[]): string => {
     .join('');
 };
 
-const getCratesSection = (lines: string[]): string[] => {
-  return lines.slice(0, lines.indexOf('')).reverse();
-};
-
-const getLineCrates = (line: string): Record<number, Crate | null> => {
-  const crates: Record<number, Crate | null> = {};
-  for (let i = 0; i < line.length; i += 4) {
-    const id = line.substring(i, i + 3);
-    const crate = /\[.\]/.test(id) ? new Crate(id) : null;
-    crates[i / 4 + 1] = crate;
+const day5Part2 = (input: Input): string => {
+  const crateStacks = CrateStack.parse(input.getCratesSection());
+  const moves = Move.parse(input.getMovesSection());
+  const crane = Crane.getCrateMover9001(crateStacks);
+  for (const move of moves) {
+    crane.move(move);
   }
-  return crates;
+
+  return crateStacks
+    .map(crateStack => crateStack.peek())
+    .map(crate => crate?.id || '')
+    .join('');
 };
 
-const getMovesSection = (lines: string[]): string[] => {
-  return lines.slice(lines.indexOf('') + 1);
-};
+class Input {
+  private _lines: string[];
+
+  constructor(lines: string[]) {
+    this._lines = lines;
+  }
+
+  get lines(): readonly string[] {
+    return this._lines;
+  }
+
+  getCratesSection(): string[] {
+    return this._lines.slice(0, this._lines.indexOf(''));
+  }
+
+  getMovesSection(): string[] {
+    return this._lines.slice(this._lines.indexOf('') + 1);
+  }
+}
 
 class Move {
   private _amount: number;
@@ -51,6 +67,10 @@ class Move {
     this._amount = parseInt(match[1]);
     this._from = parseInt(match[2]);
     this._to = parseInt(match[3]);
+  }
+
+  static parse(lines: readonly string[]): Move[] {
+    return lines.map(line => new Move(line));
   }
 
   get amount(): number {
@@ -68,19 +88,42 @@ class Move {
 
 class Crane {
   private crateStacks: CrateStack[] = [];
+  private _move: (move: Move) => void = () => {
+    throw new Error('Crane not initialized');
+  };
 
-  constructor(crateStacks: CrateStack[]) {
+  private constructor(crateStacks: CrateStack[]) {
     this.crateStacks = crateStacks;
   }
 
-  move({ amount, from, to }: Move): void {
-    const fromStack = this.crateStacks[from - 1];
-    const toStack = this.crateStacks[to - 1];
-    for (let i = 0; i < amount; i += 1) {
-      const crate = fromStack.unstack();
-      if (!crate) throw new Error('Cannot move crate from empty stack');
-      toStack.stack(crate);
-    }
+  static getCrateMover9000(crateStacks: CrateStack[]): Crane {
+    const crateMover9000 = new Crane(crateStacks);
+    crateMover9000._move = ({ amount, from, to }: Move): void => {
+      const fromStack = crateMover9000.crateStacks[from - 1];
+      const toStack = crateMover9000.crateStacks[to - 1];
+      for (let i = 0; i < amount; i += 1) {
+        const crate = fromStack.pop();
+        if (!crate) throw new Error('Cannot move crate from empty stack');
+        toStack.stack(crate);
+      }
+    };
+
+    return crateMover9000;
+  }
+
+  static getCrateMover9001(crateStacks: CrateStack[]): Crane {
+    const crateMover9001 = new Crane(crateStacks);
+    crateMover9001._move = ({ amount, from, to }: Move): void => {
+      const fromStack = crateMover9001.crateStacks[from - 1];
+      const toStack = crateMover9001.crateStacks[to - 1];
+      toStack.stack(...fromStack.take(amount));
+    };
+
+    return crateMover9001;
+  }
+
+  move(move: Move): void {
+    this._move(move);
   }
 }
 
@@ -93,10 +136,13 @@ class CrateStack {
     this._id = id;
   }
 
-  static fromString(lines: string[]): CrateStack[] {
+  static parse(lines: readonly string[]): CrateStack[] {
+    const _lines = [...lines].reverse();
     const crateStacks: Map<string, CrateStack> = new Map();
-    for (let i = 1; i < lines.length; i += 1) {
-      const crates = getLineCrates(lines[i]);
+
+    for (let i = 1; i < _lines.length; i += 1) {
+      const crates = Crate.parse(_lines[i]);
+
       for (const [stackId, crate] of Object.entries(crates)) {
         if (!crate) continue;
         const crateStack = crateStacks.get(stackId) || new CrateStack(stackId, []);
@@ -108,12 +154,16 @@ class CrateStack {
     return [...crateStacks.values()];
   }
 
-  stack(crate: Crate): void {
-    this.crates.push(crate);
+  stack(...crates: Crate[]): void {
+    this.crates.push(...crates);
   }
 
-  unstack(): Crate | undefined {
+  pop(): Crate | undefined {
     return this.crates.pop();
+  }
+
+  take(amount: number): Crate[] {
+    return this.crates.splice(this.crates.length - amount, amount);
   }
 
   peek(): Crate | undefined {
@@ -127,8 +177,19 @@ class CrateStack {
 
 class Crate {
   private _id: string;
+
   constructor(id: string) {
     this._id = id;
+  }
+
+  static parse(line: string): Record<number, Crate | null> {
+    const crates: Record<number, Crate | null> = {};
+    for (let i = 0; i < line.length; i += 4) {
+      const id = line.substring(i, i + 3);
+      const crate = /\[.\]/.test(id) ? new Crate(id) : null;
+      crates[i / 4 + 1] = crate;
+    }
+    return crates;
   }
 
   get id(): string {
